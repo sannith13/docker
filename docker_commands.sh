5f385c356f1e414fa719bc3526eb0d3cf76aba3a
sannith <sannithnag13@gmail.com>
2020-08-17 04:05:21 -0700

    changes in docker command

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,9 +1,9 @@
 # prefix commands with sudo if user is not in docker group
 # build docker image
-docker build -t hands-on-jenkins/myapp .
+docker build -t myapp .
 
 # check docker image
-docker images | grep hands-on-jenkins/myapp
+docker images | grep myapp
 
 # run docker container
-docker run -p 8888:5000 --name myapp hands-on-jenkins/myapp 
+docker run -p 8888:5000 --name myapp 
